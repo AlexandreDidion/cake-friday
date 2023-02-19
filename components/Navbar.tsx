8b854dc43569d4a05844b6b7a34cdef3cabde5e5
AlexandreDidion <ale.didion@gmail.com>
2023-02-19 18:48:12 +0100

    wip

---
+++ components/Navbar.tsx
@@ -0,0 +1,15 @@
+import styles from '@/styles/Navbar.module.scss'
+import Link from 'next/link';
+
+import Box from '@mui/material/Box'
+
+export const Navbar = () => {
+  return (
+    <>
+      <Box component="nav" className={styles.navbarContainer}>
+        <Link href="/" className={styles.link}>Cake Friday</Link>
+      </Box>
+
+    </>
+  )
+}
---
+++ pages/team/new.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+import Box from '@mui/material/Box'
+
+export const NewTeam = () => {
+  return (
+    <>
+      <Head>
+        <title>Cake - New Team</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <main>
+      </main>
+    </>
+  )
+}
