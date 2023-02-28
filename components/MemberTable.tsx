986456781f805786c739de21670e16ce6f1d6a47
AlexandreDidion <ale.didion@gmail.com>
2023-02-28 22:11:23 +0100

    basic data table mine members

---
+++ components/MemberTable.tsx
@@ -0,0 +1,39 @@
+import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid'
+
+import { MemberRow } from '@/pages/team/mine'
+
+export const MemberTable = ({rows, columns} : {rows: MemberRow[], columns: GridColDef[]}) => {
+  return (
+    <div style={{ display: 'flex', height: '100%' }}>
+      <div style={{ flexGrow: 1 }}>
+        <DataGrid
+          sx={{
+            boxShadow: 2,
+            border: 2,
+            borderColor: '#1B9AAA',
+            '& .MuiDataGrid-columnHeaders': {
+              borderColor: '#1B9AAA'
+            },
+            '& .MuiDataGrid-cell': {
+              borderColor: '#1B9AAA',
+            },
+            '& .MuiDataGrid-columnHeader:focus-within': {
+              outline: 'none',
+            },
+            '& .MuiDataGrid-footerContainer': {
+              borderColor: '#1B9AAA',
+            },
+            '& .MuiDataGrid-iconSeparator': {
+              color: '#1B9AAA',
+            }
+          }}
+          rows={rows}
+          columns={columns}
+          pageSize={5}
+          rowsPerPageOptions={[5]}
+          checkboxSelection
+        />
+      </div>
+    </div>
+  )
+}
--- pages/team/mine.tsx
+++ pages/team/mine.tsx
@@ -4,14 +4,35 @@ import { db } from '@/initFirebase'
 import { collection, query, where, getDocs, doc, DocumentData} from "firebase/firestore"
 import { memberConvertor } from '@/models/members'
 import { getCurrentUser } from '@/initFirebase'
-
+import { GridColDef } from '@mui/x-data-grid'
 import { Loader } from '@/components/Loader'
+import { MemberTable } from '@/components/MemberTable'
+import Box from '@mui/system/Box'
 
 const currentUser = await getCurrentUser()
+
+const TABLE_COLUMNS : GridColDef[] = [
+  { field: 'firstName', headerName: 'First name', width: 150 },
+  { field: 'lastName', headerName: 'Last name', width: 150 },
+  {
+    field: 'email',
+    headerName: 'Email',
+    width: 300,
+  }
+]
+
+export interface MemberRow {
+  id: number
+  firstName: string
+  lastName: string
+  email: string
+}
 
 export default function MineTeam() {
   const [isLoading, setIsLoading] = useState(false)
   const [myMembers, setMyMembers] = useState<DocumentData[]>([])
+  const [rows, setRows] = useState<MemberRow[]>([])
+  const [showTable, setShowTable] = useState(false)
 
   const getMyMembers = async () => {
     setIsLoading(true)
@@ -37,6 +58,13 @@ export default function MineTeam() {
     getMyMembers()
   }, [])
 
+  useEffect(() => {
+    const rows = myMembers.map((m, i) => {
+      return { id: i, firstName: m.firstName , lastName: m.lastName, email: m.email }
+    })
+    setRows(rows)
+    setShowTable(true)
+  }, [myMembers])
 
   return (
     <>
@@ -44,10 +72,15 @@ export default function MineTeam() {
         <title>Cake - My Team</title>
       </Head>
       {isLoading && (<Loader />)}
-      {myMembers.map((member, key) => (
-        <p key={key}>{member.fullName()}</p>
-      ))}
       <main>
+        {showTable && (
+          <Box sx={{width: "75vw", height: '75vh'}}>
+            <MemberTable
+              columns={TABLE_COLUMNS}
+              rows={rows}
+            />
+          </Box>
+        )}
       </main>
     </>
   )
